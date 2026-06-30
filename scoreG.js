dbf1149db498c571077bbe64283f43af9908f2aa
hood-darkcus <hoodhpro100@gmail.com>
2026-06-30 22:14:28 +0400

    Commit initial files

---
+++ scoreG.js
@@ -0,0 +1,15 @@
+let scoreG = 0
+document.getElementById("scoreGuest").innerText = scoreG
+
+function plus1g(){
+    scoreG += 1
+    document.getElementById("scoreGuest").innerText = scoreG
+}
+function plus2g(){
+    scoreG += 2
+    document.getElementById("scoreGuest").innerText = scoreG
+}
+function plus3g(){
+    scoreG += 3
+    document.getElementById("scoreGuest").innerText = scoreG
+}
---
+++ scoreH.js
@@ -0,0 +1,15 @@
+let scoreH = 0
+document.getElementById("scoreHome").innerText = scoreH
+
+function plus1h(){
+    scoreH += 1
+    document.getElementById("scoreHome").innerText = scoreH
+}
+function plus2h(){
+    scoreH += 2
+    document.getElementById("scoreHome").innerText = scoreH
+}
+function plus3h(){
+    scoreH += 3
+    document.getElementById("scoreHome").innerText = scoreH
+}
